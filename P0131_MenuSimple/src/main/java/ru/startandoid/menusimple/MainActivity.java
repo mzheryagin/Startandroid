48947fffe23a02dc0372deba76ec9cec05221272
mzheryagin <zheryagin@gmail.com>
2017-11-08 13:10:11 +0300

    Lession 14 in progress

---
+++ P0131_MenuSimple/src/main/java/ru/startandoid/menusimple/MainActivity.java
@@ -0,0 +1,31 @@
+package ru.startandoid.menusimple;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.*;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+  }
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    menu.add("menu1");
+    menu.add("menu2");
+    menu.add("menu3");
+    menu.add("menu4");
+
+    return super.onCreateOptionsMenu(menu);
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    Toast.makeText(this, item.getTitle(),Toast.LENGTH_LONG).show();
+    return super.onOptionsItemSelected(item);
+  }
+}
